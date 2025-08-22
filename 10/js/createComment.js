b7ab82f4807be25435ddffc498067da6f59bcf83
Keks <keks@htmlacademy.ru>
2025-08-22 17:20:57 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/createComment.js
@@ -0,0 +1,22 @@
+
+function createComment({ avatar, name, message }) {
+  const li = document.createElement('li');
+  li.classList.add('social__comment');
+
+  const img = document.createElement('img');
+  img.classList.add('social__picture');
+  img.src = avatar;
+  img.alt = name;
+  img.width = 35;
+  img.height = 35;
+
+  const p = document.createElement('p');
+  p.classList.add('social__text');
+  p.textContent = message;
+
+  li.appendChild(img);
+  li.appendChild(p);
+  return li;
+};
+
+export { createComment };
---
+++ 10/js/form/form.js
@@ -0,0 +1,73 @@
+import { MAX_HASHTAGS, HASHTAG_REGEX, MAX_COMMENT_LENGTH } from '../data/const.js';
+import { resetEffects } from './effects.js';
+import { resetScale } from './scale.js';
+import { sendData } from '../api.js';
+import { showMessage } from '../utils/messages.js';
+
+
+const form = document.querySelector('.img-upload__form');
+const uploadInput = form.querySelector('.img-upload__input');
+const overlay = form.querySelector('.img-upload__overlay');
+const cancelButton = form.querySelector('.img-upload__cancel');
+const hashTagsInput = form.querySelector('.text__hashtags');
+const descriptionInput = form.querySelector('.text__description');
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper--error'
+});
+
+function validateHashtags(value) {
+  if (!value) return true;
+  const tags = value.toLowerCase().trim().split(/\s+/);
+  if (tags.length > MAX_HASHTAGS) return false;
+  const uniqueTags = new Set(tags);
+  if (uniqueTags.size !== tags.length) return false;
+  return tags.every((tag) => HASHTAG_REGEX.test(tag));
+}
+pristine.addValidator(hashTagsInput, validateHashtags, 'Неправильные хэштеги');
+
+function validateComment(value) {
+  return value.length <= MAX_COMMENT_LENGTH;
+}
+
+pristine.addValidator(descriptionInput, validateComment, 'Комментарий не должен превышать 140 символов');
+
+uploadInput.addEventListener('change', () => {
+  overlay.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+});
+
+function closeForm() {
+  overlay.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  form.reset();
+  pristine.reset();
+  resetEffects();
+  resetScale();
+}
+
+cancelButton.addEventListener('click', closeForm);
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape' && document.activeElement !== hashTagsInput && document.activeElement !== descriptionInput
+  ) {
+    closeForm();
+  }
+});
+
+form.addEventListener('submit', async (evt) => {
+  evt.preventDefault();
+  const valid = pristine.validate();
+  if (!valid) return;
+
+  const formData = new FormData(form);
+
+  try {
+    await sendData(formData);
+    closeForm();
+    showMessage('success');
+  } catch (err) {
+    showMessage('error');
+  }
+})
